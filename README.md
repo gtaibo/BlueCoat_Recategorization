7f6b0ad1289a4596c5760bdc81cfc6f518970243
gtaibo <gtaibo@Guillermos-MBP.attlocal.net>
2017-03-11 23:12:58 -0500

    Blue Coat Site Review Recategorization
    Blue Coat Site Review Checker (CLI)
    
    Description-
    
    "The purpose of Site Review is to allow Blue Coat customers to check
    the current categorization of WebPulse URL ratings and report sites
    that they believe are incorrectly categorized."
    
    https://sitereview.bluecoat.com/sitereview.jsp
    
    Python script allows users to query the current categorization of a
    site via CLI. After querying Site Review, it provides the user with an
    option to Re-Categorize the site.
    
    Usage-
    Siterecat.py takes one argument and submits the query to get current
    categorization:
    
    example:
    
    Siterecat.py malicious.com
    *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
    Blue Coat Site Review
    *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
    
    Domain:http://malicious.com
    
    Category:Suspicious
    
    Would you like to re-categorize it? (y/n)
    y
    
    Menu
            ------------------
            1.Dynamic DNS Host
            2.Hacking
            3.Malicious Outbound Data/Botnets
            4.Malicious Sources/Malnets
            5.Mixed Content/Potentially Adult
            6.Peer-to-Peer (P2P)
            7.Phishing
            8.Pornography
            9.Potentially Unwanted Software
            10.Proxy Avoidance
            11.Remote Access Tools
            12.Suspicious
    
    Choose a number from 1 to 12:
    1
    
     Submitting as Dynamic DNS Host
    Your categorization has been submitted
    
    Python Requirements
    
    argparse
    bs4
    json
    requests
    sys

---
+++ README.md
@@ -0,0 +1,58 @@
+
+Blue Coat Site Review Checker (CLI)
+
+Description-
+
+"The purpose of Site Review is to allow Blue Coat customers to check the current categorization of WebPulse URL ratings and report sites that they believe are incorrectly categorized."
+
+https://sitereview.bluecoat.com/sitereview.jsp
+
+
+
+Python script allows users to query the current categorization of a site via CLI. After querying Site Review, it provides the user with an option to Re-Categorize the site. 
+
+Usage-
+Siterecat.py takes one argument and submits the query to get current categorization: 
+
+example: 
+
+Siterecat.py malicious.com
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+Blue Coat Site Review
+*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+
+Domain:http://malicious.com
+
+Category:Suspicious
+
+Would you like to re-categorize it? (y/n)
+y
+
+Menu
+        ------------------
+        1.Dynamic DNS Host
+        2.Hacking
+        3.Malicious Outbound Data/Botnets
+        4.Malicious Sources/Malnets
+        5.Mixed Content/Potentially Adult
+        6.Peer-to-Peer (P2P)
+        7.Phishing
+        8.Pornography
+        9.Potentially Unwanted Software
+        10.Proxy Avoidance
+        11.Remote Access Tools
+        12.Suspicious
+        
+Choose a number from 1 to 12: 
+1
+
+ Submitting as Dynamic DNS Host
+Your categorization has been submitted
+
+Python Requirements
+
+argparse
+bs4
+json
+requests
+sys
---
+++ Siterecat.py
@@ -0,0 +1,152 @@
+from argparse import ArgumentParser
+import json
+import requests
+import sys
+import cookielib
+
+
+class SiteReview(object):
+    def __init__(self):
+        self.baseurl = "http://sitereview.bluecoat.com/rest/categorization"
+        self.useragent = {"User-Agent": "Mozilla/5.0"}
+
+    def sitereview(self, url):
+        payload = {"url": url}
+        
+        try:
+            self.req = requests.post(
+                                    self.baseurl,
+                                    headers=self.useragent,
+                                    data=payload
+                                    )
+        except requests.ConnectionError:
+            sys.exit("[-] ConnectionError: " \
+                     "A connection error occurred")
+
+        return json.loads(self.req.content)
+
+    def check_response(self, response):
+
+        if self.req.status_code != 200:
+            sys.exit("[-] HTTP {} returned".format(req.status_code))
+
+        elif "error" in response:
+            sys.exit(response["error"])
+
+        else:
+            self.url = response["url"]
+            self.category = response["categorization"].split('>')[1].split('<')[0]
+            self.trackid = response["curtrackingid"]
+
+      
+
+def main(url):
+    s = SiteReview()
+    response = s.sitereview(url)
+    s.check_response(response)
+    border = "*=" * (len("BlueCoat Site Review") + 2)
+
+    print "\n{0}\n{1}\n{0}\n".format(border, "Blue Coat Site Review")
+    print ("Domain:") +s.url + ("\n")
+    print ("Category:") +s.category +("\n")
+    
+
+    bccat = ['Dynamic DNS Host','Hacking','Malicious Outbound Data/Botnets','Malicious Sources/Malnets','Mixed Content/Potentially Adult','Peer-to-Peer (P2P)','Phishing','Pornography','Potentially Unwanted Software','Proxy Avoidance','Remote Access Tools','Spam','Suspicious']
+    
+    recategorize=raw_input("Would you like to re-categorize it? (y/n)\n")
+    if recategorize == "n":
+            print("\nNow Exiting...")
+            raise SystemExit
+
+
+    if recategorize == "y" or "Y":
+          
+        print(""" 
+
+        Menu
+        ------------------
+        1.Dynamic DNS Host
+        2.Hacking
+        3.Malicious Outbound Data/Botnets
+        4.Malicious Sources/Malnets
+        5.Mixed Content/Potentially Adult
+        6.Peer-to-Peer (P2P)
+        7.Phishing
+        8.Pornography
+        9.Potentially Unwanted Software
+        10.Proxy Avoidance
+        11.Remote Access Tools
+        12.Suspicious
+        """)
+        ans=raw_input("Choose a number from 1 to 12: \n")
+
+        
+
+    
+
+    if  ans=="1":
+              print("\n Submitting as Dynamic DNS Host")
+              
+             
+    elif  ans=="2":
+              print("\n Submitting as Hacking")
+              
+             
+    elif  ans=="3":
+              print("\n Submitting as Malicious Outbound Data/Botnets")
+              
+             
+    elif  ans=="4":
+              print("\n Submitting as Malicious Sources/Malnets") 
+              ans = None
+
+    elif  ans=="5":
+              print("\n Submitting as Mixed Content/Potentially Adult")
+
+    elif  ans=="6":
+            print("\n Submitting as Peer-to-Peer (P2P)")
+    elif  ans=="7":
+              print("\n Submitting as Phishing")
+    elif  ans=="8":
+              print("\n Submitting as Pornography")
+
+    elif  ans=="9":
+              print("\n Submitting as Potentially Unwanted Software")
+
+    elif  ans=="10":
+              print("\n Submitting as Proxy Avoidance")
+    elif  ans=="11":
+              print("\n Submitting as Remote Access Tools")
+    elif  ans=="12":
+              print("\n Submitting as Suspicious")
+
+    else:
+            print("\n Not Valid Choice! Now Exiting...")
+            raise SystemExit
+
+
+
+              
+
+    trackid =  s.trackid
+    jssid = s.req.headers["set-cookie"]
+    
+    url = "http://sitereview.bluecoat.com/rest/submitCategorization"
+    origin = "http://sitereview.bluecoat.com"
+    headers= {'Cookie':jssid, 'Accept':'application/json, text/javascript','User-Agent': 'Mozilla/5.0','X-Requested-With': 'XMLHttpRequest', 'Referer': 'http://sitereview.bluecoat.com/sitereview.jsp'}
+    data= {'referrer':'bluecoatsg', 'suggestedcat':ans ,'suggestedcat2':ans ,'emailCheckBox':'on','email':'gtaibo9090@gmail.com' , 'emailcc':'','comments':'', 'overwrite':'no','trackid':trackid }
+    r = requests.post(url, data, headers=headers)
+
+    if r.status_code == 200:
+        print("Your categorization has been submitted")        
+
+
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("url", help="Submit domain/URL to Blue Coat's Site Review")
+    args = p.parse_args()
+
+    main(args.url)
+
+else:
+    pass
